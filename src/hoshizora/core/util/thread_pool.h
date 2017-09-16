982a3f4f013711e3aee9df91771462dd445170b8
amaya <mail@sapphire.in.net>
2017-09-17 00:54:03 +0900

    a

---
+++ src/hoshizora/core/util/thread_pool.h
@@ -0,0 +1,12 @@
+#ifndef HOSHIZORA_THREAD_POOL_H
+#define HOSHIZORA_THREAD_POOL_H
+
+#include "hoshizora/core/util/includes.h"
+
+namespace hoshizora {
+    struct ThreadPool {
+        ;
+    };
+}
+
+#endif //HOSHIZORA_THREAD_POOL_H
